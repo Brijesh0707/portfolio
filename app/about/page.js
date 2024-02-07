6424fe7af299edde13a41095b7f16493dc6b482f
BRIJESH <122820661+Brijesh0707@users.noreply.github.com>
2024-02-07 19:19:59 +0530

    changes about us

--- app/about/page.js
+++ app/about/page.js
@@ -1,33 +1,9 @@
 "use client"
-import React, { useEffect, useState } from 'react';
-import { motion, useAnimation } from 'framer-motion';
+import { motion } from 'framer-motion';
 import Image from 'next/image';
 
 const Page = () => {
-  const [isVisible, setIsVisible] = useState(false);
-  const controls = useAnimation();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const windowHeight = window.innerHeight;
-      const technologySection = document.getElementById('technology');
-      const technologySectionTop = technologySection.offsetTop;
-
-      if (window.scrollY + windowHeight >= technologySectionTop) {
-        setIsVisible(true);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  useEffect(() => {
-    if (isVisible) {
-      controls.start({ x: 0, opacity: 1 });
-    }
-  }, [isVisible, controls]);
+  
 
   const skills = ["html", "css", "javascript", "react js", "node js", "mongo db", "next js", "raect native", "web development", "android development"];
 
@@ -42,13 +18,13 @@ const Page = () => {
               <motion.p initial={{ x: -100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0.5, duration: 0.5 }} className='mt-4 text-center text-[#2f2f2f] font-500'>At TechTonic Studio, we're passionate about helping businesses of all sizes harness the power of technology to achieve their goals. From website design and development to digital marketing and beyond, our team of experts is dedicated to delivering top-notch solutions that exceed expectations. With a blend of creativity, expertise, and a customer-centric approach, we're committed to helping you stay ahead of the curve and thrive in the digital landscape</motion.p>
               <motion.p initial={{ x: -100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0.6, duration: 0.6 }} className='mt-4 text-center text-[#2f2f2f] font-500'>Whether you're looking to enhance your online presence, increase brand visibility, or drive conversions, TechTonic Studio has the tools and expertise to make it happen. Let's embark on this digital journey together and unlock the full potential of your business. Get in touch with us today to learn more about how we can help you succeed in the digital age.</motion.p>
             </div>
-            <div className='technology mt-[100px]' id="technology">
-              <motion.h1 initial={{ x: -100, opacity: 0 }} animate={controls} transition={{ delay: 0.3, duration: 0.3 }} className='text-[30px] text-[#2f2f2f] text-center'>Technology We Provide</motion.h1>
+            <div className='technology mt-[100px]'>
+              <motion.h1 initial={{ x: -100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0.5, duration: 0.5 }}className='text-[30px] text-[#2f2f2f] text-center'>Technology We Provide</motion.h1>
               <br />
               <br />
               <div className='flex flex-wrap gap-4'>
                 {skills?.map((item, index) => (
-                  <motion.button key={index} initial={{ x: -100, opacity: 0 }} animate={controls} transition={{ delay: index * 0.1, duration: 0.5 }} className='pl-4 pr-4 hover:bg-black hover:text-white pt-2 pb-2 rounded-lg border-black border-[1px] text-black font-500' style={{ transition: '0.5s ease-in-out' }}>{item}</motion.button>
+                  <motion.button key={index}  className='pl-4 pr-4 hover:bg-black hover:text-white pt-2 pb-2 rounded-lg border-black border-[1px] text-black font-500' style={{ transition: '0.5s ease-in-out' }}>{item}</motion.button>
                 ))}
               </div>
             </div>
